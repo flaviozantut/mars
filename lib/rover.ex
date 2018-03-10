ee76c35ae90d9b639369c099cb2021e149956d80
flaviozantut <flaviozantut@gmail.com>
2018-03-10 20:04:19 -0300

    Add limit to rover move

--- lib/rover.ex
+++ lib/rover.ex
@@ -11,34 +11,34 @@ defmodule Rover do
     [x, y, Windrose.right(point)]
   end
 
-  def move(x, y, :s) do
-    [x, y - 1, :s]
+  def move(x, y, :s, _, _) do
+    [x, if(y > 0, do: y - 1, else: y), :s]
   end
 
-  def move(x, y, :e) do
-    [x + 1, y, :e]
+  def move(x, y, :e, maxX, _) do
+    [if(x < maxX, do: x + 1, else: x), y, :e]
   end
 
-  def move(x, y, :n) do
-    [x, y + 1, :n]
+  def move(x, y, :n, _, maxY) do
+    [x, if(y < maxY, do: y + 1, else: y), :n]
   end
 
-  def move(x, y, :w) do
-    [x - 1, y, :w]
+  def move(x, y, :w, _, _) do
+    [if(x > 0, do: x - 1, else: x), y, :w]
   end
 
-  def instructions(x, y, p, [head | tail]) do
+  def instructions(x, y, p, maxX, maxY, [head | tail]) do
     [x, y, p] =
       case head do
         "l" -> Rover.left(x, y, p)
         "r" -> Rover.right(x, y, p)
-        "m" -> Rover.move(x, y, p)
+        "m" -> Rover.move(x, y, p, maxX, maxY)
       end
 
-    Rover.instructions(x, y, p, tail)
+    Rover.instructions(x, y, p, maxX, maxY, tail)
   end
 
-  def instructions(x, y, p, []) do
+  def instructions(x, y, p, _, _, []) do
     [x, y, p]
   end
 end
--- test/rover_test.exs
+++ test/rover_test.exs
@@ -11,14 +11,34 @@ defmodule RoverTest do
   end
 
   test "Rover move to east" do
-    assert Rover.move(3, 3, :e) == [4, 3, :e]
+    assert Rover.move(3, 3, :e, 5, 5) == [4, 3, :e]
+  end
+
+  test "Rover move to south limit" do
+    assert Rover.move(2, 1, :s, 5, 5) == [2, 0, :s]
+    assert Rover.move(2, 0, :s, 5, 5) == [2, 0, :s]
+  end
+
+  test "Rover move to east limit" do
+    assert Rover.move(4, 3, :e, 5, 5) == [5, 3, :e]
+    assert Rover.move(5, 3, :e, 5, 5) == [5, 3, :e]
+  end
+
+  test "Rover move to north limit" do
+    assert Rover.move(3, 4, :n, 5, 5) == [3, 5, :n]
+    assert Rover.move(3, 5, :n, 5, 5) == [3, 5, :n]
+  end
+
+  test "Rover move to west limit" do
+    assert Rover.move(1, 3, :w, 5, 5) == [0, 3, :w]
+    assert Rover.move(0, 3, :w, 5, 5) == [0, 3, :w]
   end
 
   test "Rover instructions 1 2 n  lmlmlmlmm" do
-    assert Rover.instructions(1, 2, :n, String.codepoints("lmlmlmlmm")) == [1, 3, :n]
+    assert Rover.instructions(1, 2, :n, 5, 5, String.codepoints("lmlmlmlmm")) == [1, 3, :n]
   end
 
   test "Rover instructions 3 3 e mmrmmrmrrm" do
-    assert Rover.instructions(3, 3, :e, String.codepoints("mmrmmrmrrm")) == [5, 1, :e]
+    assert Rover.instructions(3, 3, :e, 5, 5, String.codepoints("mmrmmrmrrm")) == [5, 1, :e]
   end
 end
