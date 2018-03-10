ab6bb0831596e8750094ed9dcf6d92219b5e7d9b
flaviozantut <flaviozantut@gmail.com>
2018-03-10 20:34:38 -0300

    Add rover docs

--- lib/rover.ex
+++ lib/rover.ex
@@ -3,30 +3,55 @@ defmodule Rover do
   Rover to explore mars
   """
 
+  @doc """
+  Turn rover to left
+  """
   def left(x, y, point) do
     [x, y, Windrose.left(point)]
   end
 
+  @doc """
+  Turn rover to right
+  """
   def right(x, y, point) do
     [x, y, Windrose.right(point)]
   end
 
+  @doc """
+  Move rover one position on y angle to south (stop on limit).
+  """
   def move(x, y, :s, _, _) do
     [x, if(y > 0, do: y - 1, else: y), :s]
   end
 
+  @doc """
+  Move rover one position on x angle to east (stop on limit).
+  """
   def move(x, y, :e, maxX, _) do
     [if(x < maxX, do: x + 1, else: x), y, :e]
   end
 
+  @doc """
+  Move rover one position on y angle to north (stop on limit).
+  """
   def move(x, y, :n, _, maxY) do
     [x, if(y < maxY, do: y + 1, else: y), :n]
   end
 
+  @doc """
+  Move rover one position on x angle to west (stop on limit).
+  """
   def move(x, y, :w, _, _) do
     [if(x > 0, do: x - 1, else: x), y, :w]
   end
 
+  @doc """
+  Move rover to new position by set of instructions (stop on limits).
+  ## Examples
+
+      iex> Rover.instructions(3, 3, :e, 5, 5, ["l", "m"])
+      [3, 4, :n]
+  """
   def instructions(x, y, p, maxX, maxY, [head | tail]) do
     [x, y, p] =
       case head do
