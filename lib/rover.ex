f92055d696deb0600a08ee4f56ff34cbd52ea4e7
flaviozantut <flaviozantut@gmail.com>
2018-03-10 16:52:20 -0300

    Implements rover instructions

--- lib/rover.ex
+++ lib/rover.ex
@@ -4,26 +4,41 @@ defmodule Rover do
   """
 
   def left(x, y, point) do
-    [x: x, y: y, p: Windrose.left(point)]
+    [x, y, Windrose.left(point)]
   end
 
   def right(x, y, point) do
-    [x: x, y: y, p: Windrose.right(point)]
+    [x, y, Windrose.right(point)]
   end
 
   def move(x, y, :s) do
-    [x: x, y: y - 1, p: :s]
+    [x, y - 1, :s]
   end
 
   def move(x, y, :e) do
-    [x: x + 1, y: y, p: :e]
+    [x + 1, y, :e]
   end
 
   def move(x, y, :n) do
-    [x: x, y: y + 1, p: :n]
+    [x, y + 1, :n]
   end
 
   def move(x, y, :w) do
-    [x: x - 1, y: y, p: :n]
+    [x - 1, y, :w]
+  end
+
+  def instructions(x, y, p, [head | tail]) do
+    [x, y, p] =
+      case head do
+        "l" -> Rover.left(x, y, p)
+        "r" -> Rover.right(x, y, p)
+        "m" -> Rover.move(x, y, p)
+      end
+
+    Rover.instructions(x, y, p, tail)
+  end
+
+  def instructions(x, y, p, []) do
+    [x, y, p]
   end
 end
--- test/rover_test.exs
+++ test/rover_test.exs
@@ -3,14 +3,22 @@ defmodule RoverTest do
   doctest Rover
 
   test "Rover turn left north to west" do
-    assert Rover.left(1, 2, :n) == [x: 1, y: 2, p: :w]
+    assert Rover.left(1, 2, :n) == [1, 2, :w]
   end
 
   test "Rover turn right west to north" do
-    assert Rover.right(1, 2, :w) == [x: 1, y: 2, p: :n]
+    assert Rover.right(1, 2, :w) == [1, 2, :n]
   end
 
   test "Rover move to east" do
-    assert Rover.move(3, 3, :e) == [x: 4, y: 3, p: :e]
+    assert Rover.move(3, 3, :e) == [4, 3, :e]
+  end
+
+  test "Rover instructions 1 2 n  lmlmlmlmm" do
+    assert Rover.instructions(1, 2, :n, String.codepoints("lmlmlmlmm")) == [1, 3, :n]
+  end
+
+  test "Rover instructions 3 3 e mmrmmrmrrm" do
+    assert Rover.instructions(3, 3, :e, String.codepoints("mmrmmrmrrm")) == [5, 1, :e]
   end
 end
